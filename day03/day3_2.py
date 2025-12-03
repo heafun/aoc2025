ff9a6fc66bcaf8eb9ba668261af4cf3b599ef542
heafun <marc.rosenberger100@gmail.com>
2025-12-03 12:55:27 +0100

    Day 3 Part 2

--- day03/day3_2.py
+++ day03/day3_2.py
@@ -12,6 +12,16 @@ def index_of_biggest_battery(battery_bank: str):
     
     return max_index
 
+def get_max_jolt(battery_bank: str, batteries: int) -> str:
+    max_index = index_of_biggest_battery(battery_bank[:1 - batteries] if batteries > 1 else battery_bank)
+    battery = battery_bank[max_index]
+    
+    if batteries == 1:
+        return battery
+    else:
+        return battery + get_max_jolt(battery_bank[max_index+1:], batteries - 1)    
+    
+
 file = open("day03/input1.txt")
 banks = list(map(lambda x: x.strip(), file.readlines()))
 file.close()
@@ -19,13 +29,6 @@ file.close()
 jolt_sum = 0
 
 for bank in banks:
-    max_index = index_of_biggest_battery(bank[:-1])
-    first_battery = bank[max_index]
-    
-    right_of_maxindex = bank[max_index+1:]
-    
-    second_battery = right_of_maxindex[index_of_biggest_battery(right_of_maxindex)]
-    
-    jolt_sum += int(first_battery + second_battery)
+    jolt_sum += int(get_max_jolt(bank, 12))
 
 print(jolt_sum)
