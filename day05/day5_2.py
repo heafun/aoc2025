4fd1fe38461f23d07427c31e3adbc56fc8aee038
heafun <marc.rosenberger100@gmail.com>
2025-12-05 18:16:47 +0100

    Day 5 Part 2

---
+++ day05/day5_2.py
@@ -0,0 +1,35 @@
+with open("day05/input1.txt") as file:
+    lines = [x.strip() for x in file]
+
+ranges: list[list[int]] = []
+
+for line in lines:
+    if len(line) == 0:
+        reading_ranges = False
+        break
+
+    next_range = [int(x) for x in line.split("-")]
+
+    already_included = False
+
+    for id_range in ranges.copy():
+        if id_range[0] <= next_range[0] <= next_range[1] <= id_range[1]:
+            already_included = True
+            break
+        elif id_range[0] <= next_range[0] <= id_range[1]:
+            ranges.remove(id_range)
+            next_range[0] = id_range[0]
+        elif id_range[0] <= next_range[1] <= id_range[1]:
+            ranges.remove(id_range)
+            next_range[1] = id_range[1]
+        elif next_range[0] <= id_range[0] <= id_range[1] <= next_range[1]:
+            ranges.remove(id_range)
+
+    ranges.sort(key=lambda id_range: id_range[0])
+
+    if not already_included:
+        ranges.append(next_range)
+
+count = 0
+
+print(sum([abs(id_range[0] - id_range[1]) + 1 for id_range in ranges]))
